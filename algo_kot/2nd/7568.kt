819a8a9d8cf41e10edf8b96314de2ed11a3171cb
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-07-20 01:01:55 +0900

    modified: 2nd week 7568 big one done

--- algo_kot/2nd/7568.kt
+++ algo_kot/2nd/7568.kt
@@ -1,31 +1,20 @@
-data class Meat(val w:Int, val h:Int, val o:Int){
-	var r: Int = 1
-
-	public fun comparing( b:Meat?): Int
-	{
-		return (if (w > b!!.w && h > b.h) 1 else 0)
-	}
-}
-
 fun main(args: Array<String>) {
-	var pair = arrayOfNulls<Meat>(readLine()!!.toInt())
+	var pair = arrayOfNulls<Pair<Int, Int>>(readLine()!!.toInt())
 	for (i in 0 until pair.size)
 	{
 		var splited = readLine()!!.split(' ')
-		pair[i] = Meat(splited[0].toInt(), splited[1].toInt(), i)
+		pair[i] = Pair<Int, Int>(splited[0].toInt(), splited[1].toInt())
 	}
-	pair.sortWith(Comparator{a, b ->
-		if (a!!.w > b!!.w && a.h > b.h) -1 else 0
-	})
-	for (i in 0 until pair.size - 1)
+	for (i in 0 until pair.size)
 	{
-		if (pair[i]!!.comparing(pair[i + 1]) > 0)
-			pair[i + 1]!!.r = i + 2
-		else if(i > 0)
-			pair[i + 1]!!.r = pair[i]!!.r
+		var cnt = 1
+		for (j in 0 until pair.size)
+		{
+			if (pair[i]!!.first < pair[j]!!.first && pair[i]!!.second < pair[j]!!.second)
+				cnt++
+		}
+		print(cnt)
+		if (i < pair.size - 1)
+			print(" ")
 	}
-	pair.sortWith(compareBy({it!!.o}))
-	for (i in 0 until pair.size - 1)
-		print("${pair[i]!!.r} ")
-	print("${pair[pair.size - 1]!!.r}")
 }
