de9395d4b0d86c98cd472798c6ba9fb0edf0f55f
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-07-24 20:12:20 +0900

    modified: 2503 nuber baseball done

--- algo_kot/3rd/2503.kt
+++ algo_kot/3rd/2503.kt
@@ -1,34 +1,52 @@
 data class BaseBall(val num:Int, val strike:Int, val ball:Int)
 {
-	val set:Set<Int> = setOf<Int>(num / 100, (num / 10) % 10, num % 10)
+	val first = num / 100
+	val second = (num / 10) % 10
+	val third = num % 10
+	val set:Set<Int> = setOf<Int>(first, second, third)
 	val sum = strike + ball
 }
-
+fun checkBaseBall(bB:Array<BaseBall?>, i:Int, j:Int, k:Int):Boolean
+{
+	for (l in 0 until bB.size) {
+		var tmp = bB[l]!!
+		var strikeCnt = 0
+		var ballCnt = 0
+		if (i == tmp.first)
+			strikeCnt++
+		else if (tmp.set.contains(i))
+			ballCnt++
+		if (j == tmp.second)
+			strikeCnt++
+		else if (tmp.set.contains(j))
+			ballCnt++
+		if (k == tmp.third)
+			strikeCnt++
+		else if (tmp.set.contains(k))
+			ballCnt++
+		if (tmp.strike != strikeCnt || tmp.ball != ballCnt)
+			return false
+	}
+	return true
+}
 fun main(args: Array<String>) {
-	var bB = arrayOfNulls<BaseBall>(readLine()!!.toInt())
+	var bB:Array<BaseBall?> = arrayOfNulls<BaseBall>(readLine()!!.toInt())
 	var input:List<String>
-	var guess:Set<Int> = mutableSetOf()
-	var young:Array<Int> = arrayOf(3)
+	var cnt = 0
+
 	for (i in 0 until bB.size) {
 		input = readLine()!!.split(" ")
 		bB[i] = BaseBall(input[0].toInt(), input[1].toInt(), input[2].toInt())
 	}
-	for (i in 0 until bB.size) {
-		for (j in i until bB.size) {
-			if (i != j)
-			{
-				if(bB[i]!!.sum == 3)
-				else if(bB[i]!!.sum == 2 && bB[j]!!.sum == 2 )
-				{
-					var tmp = bB[i]!!.set.intersect(bB[j]!!.set)
-					guess.union(tmp)
-					guess.subtract(bB[i]!!.set.union(bB[j]!!.set).subtract(tmp))
-				}
-				else if()
-					print("${bB[i]!!.strike + bB[i]!!.ball} ${bB[j]!!.strike + bB[j]!!.ball}")
-					println(bB[i]!!.set.intersect(bB[j]!!.set))
+	for (i in 1 .. 9) {
+		for (j in 1 .. 9) {
+			if (i == j) continue
+			for (k in 1 .. 9) {
+				if (k == i || k == j) continue
+				if (checkBaseBall(bB, i, j, k))
+					cnt++
 			}
 		}
 	}
-	println(guess.size)
+	println(cnt)
 }
