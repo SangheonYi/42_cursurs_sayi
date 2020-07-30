69d862205119281adffc3716fba127f4f13baec9
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-07-30 21:29:36 +0900

    modified: 4706 twin prodox done

--- algo_kot/4th/4706.kt
+++ algo_kot/4th/4706.kt
@@ -7,6 +7,15 @@
 // out
 // 0.866
 // 0.980
+import kotlin.math.pow
 fun main(args: Array<String>) {
-
+	var input = readLine()!!.split(' ').map { it.toDouble() }
+	var tatb = mutableListOf<Double>(input[0], input[1])
+	while (tatb[0] != 0.0 && tatb[1] != 0.0)
+	{
+		println("%.3f".format((1 - (tatb[1] / tatb[0]).pow(2)).pow(1 / 2.0)))
+		input = readLine()!!.split(' ').map { it.toDouble() }
+		tatb[0] = input[0]
+		tatb[1] = input[1]
+	}
 }
