e0cf41feb246872186528a0fbd821e3d90049cb6
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-07-30 16:41:27 +0900

    modified: 10994 print square with star done

--- algo_kot/4th/10994.kt
+++ algo_kot/4th/10994.kt
@@ -5,34 +5,40 @@ fun main(args: Array<String>) {
 	var outPut = arrayListOf<String>()
 	var even = StringBuilder()
 	var odd = StringBuilder()
+	// var result = StringBuilder()
 
 	while (++i < edge)
 	{
 		even.append('*')
 		odd.append(' ')
 	}
-	i = -1
+	outPut.add(even.toString())
+	i = 0
 	while (++i <= edge / 2)
 	{
 		if (i % 2 == 0)
 		{
-			if (i % 2 == 1)
-			{
-				even.setCharAt(i - 1, ' ')
-				even.setCharAt(even.lastIndex - (i - 1), ' ')
-			}
+			even.setCharAt(i - 1, ' ')
+			even.setCharAt(even.lastIndex - (i - 1), ' ')
 			outPut.add(even.toString())
-			//출력용 builder도 새로 만들어 주자
 		}
 		else
 		{
-			if (i % 2 == 1)
-			{
-				odd.setCharAt(i - 1, '*')
-				odd.setCharAt(odd.lastIndex - (i - 1), '*')
-			}
+			odd.setCharAt(i - 1, '*')
+			odd.setCharAt(odd.lastIndex - (i - 1), '*')
 			outPut.add(odd.toString())
 		}
 	}
-	println(buildLine())
+	i = -1
+	while(++i < outPut.size)
+	{
+		print("$i: ")
+		println(outPut[i])
+	}
+	i = outPut.size - 1
+	while(--i >= 0)
+	{
+		print("$i: ")
+		println(outPut[i])
+	}
 }
