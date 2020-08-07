119d0c3ef7517091bdf00790000d27f8daad808a
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-07 15:38:58 +0900

    modified: 10989 ascendign memory overflow

--- algo_kot/5th/10989.kt
+++ algo_kot/5th/10989.kt
@@ -1,17 +1,23 @@
+fun increment(map: MutableMap<Int, Int>, key: Int) {
+	map.putIfAbsent(key, 0)
+	map[key] = map[key]!! + 1
+}
+
 fun main() {
-	var input = arrayListOf<Int>()
+	var mMap = mutableMapOf<Int, Int>()
 	var i = -1
-	val j = readLine()!!.toInt()
+	var j = readLine()!!.toInt()
 	val output = StringBuilder()
 	while (++i < j)
-		input.add(readLine()!!.toInt())
-	input.sort()
-	i = -1
-	while (++i < j)
-	{
-		output.append(input[i])
-		output.append("\n")
-	}
+		increment(mMap, readLine()!!.toInt())
+	var iter = mMap.keys.iterator()
+	iter.forEach {
+		while(mMap[it]!! > 0)
+		{
+			output.append("$it\n")
+			mMap[it] = mMap[it]!! - 1
+		}
+	 }
 	output.deleteCharAt(output.length - 1)
 	println(output)
 }
