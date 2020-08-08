f444dfec26511924742142a466f9b968677cfd7d
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-08 21:20:33 +0900

    add: intellij proj

---
+++ algo_kot/5th/1260.kt
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*
+
+fun readInts() = readLine().split(' ').map{ it.toInt() }
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    var attribute = readInts()
+    val stack = ArrayDeque()
+
+
+}
