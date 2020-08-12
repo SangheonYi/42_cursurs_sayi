f3479ece9434b18876007036ce1aac0c67be3f85
sayi <sayi@student.42seoul.kr>
2020-08-12 22:11:54 +0900

    test: drawing lines

--- cub3d/test.c
+++ cub3d/test.c
@@ -8,20 +8,32 @@ typedef struct  s_data {
     int   endian;
 }            t_data;
 
-int  main(void)
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int             main(void)
 {
     void    *mlx;
+    void    *mlx_win;
     t_data  img;
 
     mlx = mlx_init();
-    img.img = mlx_new_image(mlx, 1920, 1080);
-
-    /*
-    ** After creating an image, we can call `mlx_get_data_addr`, we pass
-    ** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-    ** then be set accordingly for the *current* data address.
-    */
+    mlx_win = mlx_new_window(mlx, 400, 200, "Hello world!");
+    img.img = mlx_new_image(mlx, 400, 200);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                  &img.endian);
-    printf("%p, %d, %d, %d", img.addr, img.bits_per_pixel, img.line_length, img.endian);
+    for (int i = 0; i < 100; i++)
+    {
+        my_mlx_pixel_put(&img, 5, i, 0x00FFFF00);
+        my_mlx_pixel_put(&img, i, i, 0x00FFFF00);
+        my_mlx_pixel_put(&img, i, 5, 0x00FFFF00);
+    }
+
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
 }
