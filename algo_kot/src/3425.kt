ca8645c0d8506e0e1aa0b1cb0bd637cdc15dfb0f
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-16 17:03:16 +0900

    modified: 3425 failed

--- algo_kot/src/3425.kt
+++ algo_kot/src/3425.kt
@@ -1,9 +1,10 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
-import java.math.BigInteger
 import java.util.ArrayDeque
-class Gostack
+import kotlin.math.absoluteValue
+
+class GoStack
 {
     var program = arrayListOf<String>()
     var input = arrayListOf<String>()
@@ -12,25 +13,29 @@ class Gostack
 
     fun runProgram()
     {
-        var firstIn: Int
-        var result = ""
+        var result: Int
 
         for (i in 1 .. input[0].toInt())
         {
-            firstIn = input[i].toInt()
-            stack.push(firstIn)
-            for (j in 0 until program.size)
+            result = input[i].toInt()
+            stack.push(result)
+            for (j in 0 until program.size - 1)
             {
                 result = calculate(program[j])
-                if (result.equals("ERROR"))
-                    break
+                if (result.absoluteValue >= 1000000000) break
             }
-            output.append("$result\n")
+            stack.pop()
+            if (result.absoluteValue < 1000000000 && stack.isEmpty()) output.append("$result\n")
+            else output.append("ERROR\n")
+            stack.clear()
         }
     }
-    fun calculate(op:String): String
+    fun calculate(op:String): Int
     {
-        var tmp = op.split(" ")
+        val tmp = op.split(" ")
+        val tmpInt: Int
+        val tmpInt2: Int
+
         when (tmp[0])
         {
             //NUM X: X를 스택의 가장 위에 저장한다. (0 ≤ X ≤ 109)
@@ -42,19 +47,46 @@ class Gostack
             //DUP: 첫 번째 숫자를 하나 더 스택의 가장 위에 저장한다.
             "DUP"-> stack.push(stack.first)
             //SWP: 첫 번째 숫자와 두 번째 숫자의 위치를 서로 바꾼다.
-            "SWP"
+            "SWP"->{
+                tmpInt = stack.pop()
+                tmpInt2 = stack.pop()
+                stack.push(tmpInt)
+                stack.push(tmpInt2)
+            }
             //ADD: 첫 번째 숫자와 두 번째 숫자를 더한다.
+            "ADD"-> stack.push(stack.pop() + stack.pop())
             //SUB: 첫 번째 숫자와 두 번째 숫자를 뺀다. (두 번째 - 첫 번째)
+            "SUB"-> {
+                tmpInt = stack.pop()
+                tmpInt2 = stack.pop()
+                stack.push(tmpInt2 - tmpInt)
+            }
             //MUL: 첫 번째 숫자와 두 번째 숫자를 곱한다.
+            "MUL"-> {
+                tmpInt = stack.pop()
+                tmpInt2 = stack.pop()
+                stack.push(tmpInt2 * tmpInt)
+            }
             //DIV: 첫 번째 숫자로 두 번째 숫자를 나눈 몫을 저장한다. 두 번째 숫자가 피제수, 첫 번째 숫자가 제수이다.
+            "DIV"-> {
+                tmpInt = stack.pop()
+                tmpInt2 = stack.pop()
+                stack.push(tmpInt2 / tmpInt)
+            }
             //MOD: 첫 번째 숫자로 두 번째 숫자를 나눈 나머지를 저장한다. 두 번째 숫자가 피제수, 첫 번째 숫자가 제수이다.
+            "MOD"-> {
+                tmpInt = stack.pop()
+                tmpInt2 = stack.pop()
+                stack.push(tmpInt2 % tmpInt)
+            }
+            else -> return 0
         }
-        return "ERROR"
+        return stack.first
     }
 }
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val go = Gostack()
+    val go = GoStack()
     var tmp = readLine()
 
     while (!tmp.equals("QUIT"))
@@ -74,8 +106,8 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
             tmp = readLine()
         }
         go.input.add(tmp)
-
         go.runProgram()
+        go.output.append('\n')
         tmp = readLine()
     }
     println(go.output)
