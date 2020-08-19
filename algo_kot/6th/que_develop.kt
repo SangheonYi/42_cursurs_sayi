e655980b859ae69877e2064b073513b9c4d9b2de
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-19 23:35:11 +0900

    modified: que_develop done

--- algo_kot/6th/que_develop.kt
+++ algo_kot/6th/que_develop.kt
@@ -4,18 +4,13 @@ class Solution {
         val days = IntArray(progresses.size)
         var dDay:Int
 
-        if ((100 - progresses[0]) % speeds[0] > 0)
-            days[0] = (100 - progresses[0]) / speeds[0] + 1
-        else
-            days[0] = (100 - progresses[0]) / speeds[0]
+        days[0] = (100 - progresses[0]) / speeds[0]
+        if ((100 - progresses[0]) % speeds[0] > 0) days[0]++
         dDay = days[0]
         for (i in 1 .. progresses.lastIndex)
         {
             days[i] = (100 - progresses[i]) / speeds[i]
-            if ((100 - progresses[i]) % speeds[i] > 0)
-                days[i] = (100 - progresses[i]) / speeds[i] + 1
-            else
-                days[i] = (100 - progresses[i]) / speeds[i]
+            if ((100 - progresses[i]) % speeds[i] > 0) days[i]++
             if (dDay >= days[i])
                 answer[answer.lastIndex]++
             else
