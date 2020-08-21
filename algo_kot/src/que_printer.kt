ca3d678898d48b8fafc0a63a317ad289f59b5695
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-21 21:45:48 +0900

    modified: que_printer success

--- algo_kot/src/que_printer.kt
+++ algo_kot/src/que_printer.kt
@@ -3,34 +3,38 @@ import java.util.ArrayDeque
 class Solution {
     fun solution(priorities: IntArray, location: Int): Int {
         var answer = 0
-        var maxPrior: Int
-        val doc: ArrayDeque<Int>
+        var ele:Int
+        val doc = ArrayDeque<Int>()
         val targetPrior = priorities[location]
+        var maxPrior = priorities.max()!!
 
-        priorities[location] = -1
-        doc = ArrayDeque(priorities.toList())
-        maxPrior = doc.maxOf { it }
-        println(maxPrior)
-        while (maxPrior > targetPrior) {
-            if (doc.first != maxPrior) {
-                doc.add(doc.poll())
-                answer++
-            } else {
+//        priorities[location] = -1
+        priorities.mapIndexed { idx, e ->
+            doc.add(e)
+            if (idx == location) doc.add(-1)
+        }
+        println("maxPrior: $maxPrior ")
+        doc.forEach { i -> print("$i ") }
+        println()
+        while (maxPrior >= targetPrior) {
+            ele = doc.poll()
+            if (ele != maxPrior ) {
+                if (ele == -1 && targetPrior == maxPrior) break
+                doc.add(ele)
+            }
+            else {
 //                doc.poll()
 //                println("que: $doc, max: ${doc.maxOf { it }}, polled: ${doc.poll()}, polled que: $doc")
-                maxPrior = doc.maxOf { it }
+                maxPrior = doc.max()!!
+                answer++
             }
         }
-        while (doc.first != -1) {
-            doc.add(doc.poll())
-            answer++
-        }
-        return answer + 1
+        return answer
     }
 }
 
 fun main() {
     val s = Solution()
 
-    println(s.solution(intArrayOf(2, 1, 3, 2), 1))
+    println(s.solution(intArrayOf(2, 1, 3, 2), 2))
 }
