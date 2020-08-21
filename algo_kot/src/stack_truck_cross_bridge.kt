ddfba1b4e6613f917196bbafc6c7692b06880a77
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-21 10:29:01 +0900

    modified: stack truck cross bridge 2ndtry

--- algo_kot/src/stack_truck_cross_bridge.kt
+++ algo_kot/src/stack_truck_cross_bridge.kt
@@ -4,22 +4,30 @@ class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
         var answer = 0
         var i = 0
-        var passed = 0
         var weightStat = 0
-        val progress = ArrayDeque<Int>()
+        val progress = ArrayDeque<Pair<Int, Int>>()
 
-        while (passed < truck_weights.size)
-        {
-            if (progress.size == bridge_length && progress.poll() > 0) passed++
-            if (i < truck_weights.size && weightStat + truck_weights[i] < weight)
-            {
-                progress.add(truck_weights[i])
-                weightStat += truck_weights[i++]
+        while (i < truck_weights.size) {
+            if (progress.size > 0 && progress.first.second > answer) {
+                weightStat -= progress.first.first
+                progress.remove()
             }
-            else
-                progress.add(0)
-            answer++
+            if (i < truck_weights.size && weightStat + truck_weights[i] < weight) {
+                progress.add(Pair(truck_weights[i], answer + bridge_length + 1))
+                weightStat += truck_weights[i++]
+                answer++
+            } else if (progress.size > 0)
+                answer = progress.first.second - 1
+            if (i > truck_weights.lastIndex)
+                answer = progress.last.second
+            println("answer: $answer, progress: $progress, i:$i")
         }
         return answer
     }
+}
+
+fun main() {
+    val s = Solution()
+
+    println(s.solution(	2, 10, intArrayOf(7, 4, 5, 6)))
 }
