384997f357ce3e5ecfec2fcaf2752fc91a6a1c5b
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-25 09:43:35 +0900

    modified: SortKthNum done

--- algo_kot/7th/SortKthNum.kt
+++ algo_kot/7th/SortKthNum.kt
@@ -1,6 +1,24 @@
+import kotlin.collections.ArrayList
+
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-        var answer = intArrayOf()
+        val answer = IntArray(commands.size) { 0 }
+        val list = ArrayList<Int>()
+
+        for (i in 0..commands.lastIndex) {
+            for (j in commands[i][0] - 1 until commands[i][1]) list.add(array[j])
+            list.sort()
+//            println("i:$i, k: ${commands[i][2]}")
+            answer[i] = list[commands[i][2] - 1]
+            list.clear()
+        }
         return answer
+    }
+}
+
+fun main() {
+    val s = Solution().solution(intArrayOf(1, 5, 2, 6, 3, 7, 4), arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3)))
+    s.forEach { i ->
+        println(i)
     }
 }
--- algo_kot/src/SortKthNum.kt
+++
@@ -1,6 +0,0 @@
-class Solution {
-    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-        var answer = intArrayOf()
-        return answer
-    }
-}
