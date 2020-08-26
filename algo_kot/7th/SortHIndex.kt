c4c95270b130d33986084c5f53521d7cc9cee582
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-26 18:08:43 +0900

    modified: SortHIndex done

--- algo_kot/7th/SortHIndex.kt
+++ algo_kot/7th/SortHIndex.kt
@@ -1,6 +1,13 @@
 class Solution {
     fun solution(citations: IntArray): Int {
-        var answer = 0
-        return answer
+        var h = 1
+
+        citations.sort()
+        for (i in citations.lastIndex downTo 0) {
+            if (citations[i] >= h &&
+                (i == 0 || (i > 0 && citations[i - 1] <= h))) return h
+            h++
+        }
+        return 0
     }
 }
