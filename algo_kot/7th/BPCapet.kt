52f817ed63b56a7ef4f8008c141b638639acb511
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-29 15:59:53 +0900

    modified: BPCapet done

--- algo_kot/7th/BPCapet.kt
+++ algo_kot/7th/BPCapet.kt
@@ -1,6 +1,17 @@
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
-        var answer = intArrayOf()
-        return answer
+        var x = 3
+        var y = 3
+
+        while (x * y != brown + yellow ||
+            brown != 2 * x + 2* y - 4 ||
+            yellow != (x - 2) * (y - 2)) {
+            if (x > y) y++
+            else {
+                y = 3
+                x++
+            }
+        }
+        return intArrayOf(x, y)
     }
 }
