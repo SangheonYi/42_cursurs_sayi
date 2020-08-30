8ce303b05c713a9eddc9929ab0a2470e925b328b
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-30 19:50:35 +0900

    modified: GreedyGymSuit done

--- algo_kot/7th/GreedyGymSuit.kt
+++ algo_kot/7th/GreedyGymSuit.kt
@@ -1,7 +1,46 @@
 class Solution {
-    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        var answer = 0
 
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = n - lost.size
+        var i = 0
+        val lostSet = lost.toHashSet()
+        val able = arrayListOf<Int>()
+
+        reserve.forEach { e ->
+            if (lostSet.contains(e)) {
+                lostSet.remove(e)
+                answer++
+            }
+            else able.add(e)
+        }
+        able.sort()
+        while (able.isNotEmpty())
+        {
+            var left = false
+            var right = false
+            val e = able[i]
+
+            if (lostSet.contains(e - 1)) left = true
+            if (lostSet.contains(e + 1)) right = true
+            if ((left && right && !able.contains(e + 2)) || (!left && right)) {
+                lostSet.remove(e + 1)
+                able.remove(e)
+                answer++
+            }
+            else if ((left && right && !able.contains(e - 2)) || (left && !right)) {
+                lostSet.remove(e - 1)
+                able.remove(e)
+                answer++
+            }
+            else if (!left && !right)
+                able.remove(e)
+            if (i < able.lastIndex) i++ else i = 0
+        }
         return answer
     }
+}
+
+fun main() {
+    val s = Solution()
+    println(s.solution(5,	intArrayOf(2, 4), intArrayOf(1, 3)))
 }
