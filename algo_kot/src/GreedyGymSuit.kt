367c80091207f73afcd0bc23d0418fec98b66671
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-08-30 19:27:03 +0900

    modified: GreedyGymSuit other is slower than mine

--- algo_kot/src/GreedyGymSuit.kt
+++ algo_kot/src/GreedyGymSuit.kt
@@ -1,42 +1,18 @@
 class Solution {
 
     fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        var answer = n - lost.size
-        var i = 0
-        val lostSet = HashSet<Int>()
-        val able = arrayListOf<Int>()
+        val lostArray = (lost.toSet() - reserve.toSet()).toIntArray()
+        val able = ArrayList(reserve.toSet() - lost.toSet())
+        var answer = n
 
-        lost.forEach { e -> lostSet.add(e) }
-        reserve.forEach { e ->
-            if (lostSet.contains(e)) {
-                lostSet.remove(e)
-                answer++
-            }
-            else able.add(e)
-        }
-        able.sort()
-        println("lostSet.size: ${lostSet.size}, lost.size: ${lost.size}")
-        while (able.isNotEmpty())
+        lostArray.sort()
+        for (v in lostArray)
         {
-            var left = false
-            var right = false
-            val e = able[i]
-
-            if (lostSet.contains(e - 1)) left = true
-            if (lostSet.contains(e + 1)) right = true
-            if ((left && right && !able.contains(e + 2)) || (!left && right)) {
-                lostSet.remove(e + 1)
-                able.remove(e)
-                answer++
+            when {
+                able.contains(v + 1) -> able.remove(v + 1)
+                able.contains(v - 1) -> able.remove(v - 1)
+                else -> answer--
             }
-            else if ((left && right && !able.contains(e - 2)) || (left && !right)) {
-                lostSet.remove(e - 1)
-                able.remove(e)
-                answer++
-            }
-            else if (!left && !right)
-                able.remove(e)
-            if (i < able.lastIndex) i++ else i = 0
         }
         return answer
     }
