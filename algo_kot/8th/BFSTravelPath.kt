bfc8d5292603453517ab8fd8a44e811e7ad6ef1c
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-06 09:37:43 +0900

    modified: commit modifing 8th BFSTravelPath.kt not done

--- algo_kot/8th/BFSTravelPath.kt
+++ algo_kot/8th/BFSTravelPath.kt
@@ -2,7 +2,7 @@ fun solution(tickets: Array<Array<String>>): Array<String> {
     var answer = arrayOf<String>()
     return answer
 }
-
+//[[ICN, JFK], [HND, IAD], [JFK, HND]
 fun main() {
-    println(solution())
+    println(solution(arrayOf(arrayOf("ICN", "JFK"), arrayOf("HND", "IAD"), arrayOf("JFK", "HND"))))
 }
