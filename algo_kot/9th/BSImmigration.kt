7c84b59a24a73462c8f5122190eafd1afd0ee5da
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-07 20:07:09 +0900

    add: 9th BSImmigration.kt

---
+++ algo_kot/9th/BSImmigration.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(n: Int, times: IntArray): Long {
+        var answer: Long = 0
+        return answer
+    }
+}
--- algo_kot/src/test.kt
+++ algo_kot/src/test.kt
@@ -1,8 +1,16 @@
 
 fun main() {
-    val foo = "sayi".toHashSet()
-    val foo2 = "yi".toHashSet()
-    (foo - foo2).forEach {
-        println(it)
-    }
+    val foo = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+//    val foo = arrayOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+    val hs = ArrayList<Array<String>>()
+//    for (i in 0..foo.lastIndex) { hs.add( foo[i].copyOf()) }
+//    hs[0] = arrayOf("ICN", "B")
+    hs.addAll(foo)
+    hs[0][0] = "chan"
+    foo.forEach { print(it.contentToString()) }
+    println()
+    hs.forEach { print(it.contentToString()) }
+//    array DeeptoString으로 알맹이 출력 쉽다.
+//    println(foo.contentToString())
+//    println(foo.contentDeepToString())
 }
