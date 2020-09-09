69f34ef79b591d4898843db20b02bb16604372b6
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-09 10:36:01 +0900

    add: 9th GraphDeepestNode.kt

---
+++ algo_kot/9th/GraphDeepestNode.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int, edge: Array<IntArray>): Int {
+        var answer = 0
+        return answer
+    }
+}
+
+fun main() {
+    val s = Solution()
+    println(s.solution())
+}
--- algo_kot/src/test.kt
+++ algo_kot/src/test.kt
@@ -1,16 +1,43 @@
 
 fun main() {
-    val foo = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
-//    val foo = arrayOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
-    val hs = ArrayList<Array<String>>()
-//    for (i in 0..foo.lastIndex) { hs.add( foo[i].copyOf()) }
-//    hs[0] = arrayOf("ICN", "B")
-    hs.addAll(foo)
-    hs[0][0] = "chan"
-    foo.forEach { print(it.contentToString()) }
-    println()
-    hs.forEach { print(it.contentToString()) }
-//    array DeeptoString으로 알맹이 출력 쉽다.
-//    println(foo.contentToString())
-//    println(foo.contentDeepToString())
+    val src1 = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+    val src2 = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+    val src3 = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+    val src4 = arrayListOf(arrayOf("ICN", "A"), arrayOf("ICN", "B"), arrayOf("B", "ICN"))
+    val copiedAddAll = ArrayList<Array<String>>()
+    val copiedForEachAdd = ArrayList<Array<String>>()
+    val copiedConstructWithSrc = ArrayList<Array<String>>(src3)
+    val copiedForEachAddCopyOf = ArrayList<Array<String>>()
+
+    copiedAddAll.addAll(src1) // addAll
+    src2.forEach { copiedForEachAdd.add(it) } // for each add
+    src4.forEach { copiedForEachAddCopyOf.add(it.copyOf()) }// for each add copy of
+
+    copiedAddAll[0][0] = "changed"
+    copiedForEachAdd[0][0] = "changed"
+    copiedConstructWithSrc[0][0] = "changed"
+    copiedForEachAddCopyOf[0][0] = "changed"
+
+    println("Shallow copy")
+    println("Add all")
+    print("src: ")
+    src1.forEach { print(it.contentToString()) }
+    print("\ncopied: ")
+    copiedAddAll.forEach { print(it.contentToString()) }
+    println("\n\nFor each add")
+    print("src: ")
+    src2.forEach { print(it.contentToString()) }
+    print("\ncopied: ")
+    copiedForEachAdd.forEach { print(it.contentToString()) }
+    println("\n\nConstruct with src")
+    print("src: ")
+    src3.forEach { print(it.contentToString()) }
+    print("\ncopied: ")
+    copiedConstructWithSrc.forEach { print(it.contentToString()) }
+    println("\n\nDeep copy")
+    println("For each add copy of")
+    print("src: ")
+    src4.forEach { print(it.contentToString()) }
+    print("\ncopied: ")
+    copiedForEachAddCopyOf.forEach { print(it.contentToString()) }
 }
