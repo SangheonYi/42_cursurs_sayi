875eff7017ca55e2b8c2ccabbef2475ab3d20601
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-17 08:27:15 +0900

    add: rest chapter & done

--- miniRT/C12/main.cc
+++ miniRT/C12/main.cc
@@ -3,6 +3,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
 
 #include <iostream>
 color ray_color(const ray &r, const hittable &world, int depth)
@@ -14,10 +15,12 @@ color ray_color(const ray &r, const hittable &world, int depth)
 		return color(0, 0, 0);
 
 	if (world.hit(r, 0.001, infinity, rec))
-
 	{
-		point3 target = rec.p + random_in_hemisphere(rec.normal);
-		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+		ray scattered;
+		color attenuation;
+		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+			return attenuation * ray_color(scattered, world, depth - 1);
+		return color(0, 0, 0);
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
@@ -37,12 +40,32 @@ int main()
 	const int max_depth = 50;
 
 	// World
+
+	auto R = cos(pi / 4);
 	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// Camera
-	camera cam;
+
+	point3 lookfrom(3, 3, 2);
+	point3 lookat(0, 0, -1);
+	// point3 lookfrom(0, 0, 0.5);
+	// point3 lookat(0, 0, -1);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).length();
+	auto aperture = 2.0;
+
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 	// Render
 
@@ -52,6 +75,7 @@ int main()
 	for (int j = image_height - 1; j >= 0; --j)
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
 		for (int i = 0; i < image_width; ++i)
 		{
 			color pixel_color(0, 0, 0);
