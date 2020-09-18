784d25a81057de3f1c36697508eb9bcb6701489d
sayi <sayi@student.42seoul.com>
2020-09-18 23:53:11 +0900

    modified: buffered out

--- miniRT/C13/main.cc
+++ miniRT/C13/main.cc
@@ -4,8 +4,12 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include <string.h>
+#include <stdlib.h>
 
 #include <iostream>
+using namespace std;
+
 color ray_color(const ray &r, const hittable &world, int depth)
 {
 	hit_record rec;
@@ -34,7 +38,7 @@ hittable_list random_scene()
 
 	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
 	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-
+/*
 	for (int a = -11; a < 11; a++)
 	{
 		for (int b = -11; b < 11; b++)
@@ -70,7 +74,7 @@ hittable_list random_scene()
 			}
 		}
 	}
-
+ */
 	auto material1 = make_shared<dielectric>(1.5);
 	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
@@ -89,10 +93,11 @@ int main()
 	// Image
 
 	const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 1200;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 500;
 	const int max_depth = 50;
+	string buffer = "";
 
 	// World
 
@@ -110,11 +115,12 @@ int main()
 
 	// Render
 
-	std::cout << "P3\n"
-			  << image_width << ' ' << image_height << "\n255\n";
+	// std::cout << "P3\n"
+	// 		  << image_width << ' ' << image_height << "\n255\n";
+	buffer += "P3\n" + to_string(image_width) + ' ' + to_string(image_height) + "\n255\n";
 
-	std::cerr << "P3\n"
-			  << image_width << ' ' << image_height << "\n255\n";
+	std::cerr << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
 	for (int j = image_height - 1; j >= 0; --j)
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -129,9 +135,9 @@ int main()
 				ray r = cam.get_ray(u, v);
 				pixel_color += ray_color(r, world, max_depth);
 			}
-			write_color(std::cout, pixel_color, samples_per_pixel);
+			write_color(buffer, pixel_color, samples_per_pixel);
 		}
 	}
-
+	std::cout << buffer;
 	std::cerr << "\nDone.\n";
 }
