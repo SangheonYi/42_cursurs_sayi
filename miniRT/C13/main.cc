4510a0042842d32e33f2c325e0bf973a6a0bd957
math104 <math104@alpha-math.hufs.ac.kr>
2020-09-19 00:20:58 +0900

    testing: buffer out

--- miniRT/C13/main.cc
+++ miniRT/C13/main.cc
@@ -93,9 +93,9 @@ int main()
 	// Image
 
 	const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 400;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 50;
+	const int samples_per_pixel = 500;
 	const int max_depth = 50;
 	string buffer = "";
 
