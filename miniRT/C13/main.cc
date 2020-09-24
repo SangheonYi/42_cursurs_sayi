6ea8ceab9154fc02ddebce988cfbf4b04394d2cc
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-24 18:40:58 +0900

    testing

--- miniRT/C13/main.cc
+++ miniRT/C13/main.cc
@@ -15,7 +15,7 @@ using namespace std;
 
 // Image
 const auto aspect_ratio = 3.0 / 2.0;
-const int image_width = 100;
+const int image_width = 200;
 const int image_height = static_cast<int>(image_width / aspect_ratio);
 const int samples_per_pixel = 50;
 const int max_depth = 20;
@@ -146,7 +146,7 @@ int main()
 	std::cerr << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	//thread
-	int thread_num = 25;
+	int thread_num = 10;
 	vector<thread> threads;
 	vector<string> buffers(thread_num + 1);
 
