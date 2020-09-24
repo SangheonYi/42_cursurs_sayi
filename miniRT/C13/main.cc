2fac6f85a913f73e91d7e51da470f8fb26375462
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-24 18:54:45 +0900

    testing

--- miniRT/C13/main.cc
+++ miniRT/C13/main.cc
@@ -15,10 +15,10 @@ using namespace std;
 
 // Image
 const auto aspect_ratio = 3.0 / 2.0;
-const int image_width = 200;
+const int image_width = 1200;
 const int image_height = static_cast<int>(image_width / aspect_ratio);
-const int samples_per_pixel = 50;
-const int max_depth = 20;
+const int samples_per_pixel = 500;
+const int max_depth = 50;
 auto dist_to_focus = 1.0;
 auto aperture = 0.001;
 
@@ -53,7 +53,7 @@ hittable_list random_scene()
 	{
 		for (int b = -11; b < 11; b++)
 		{
-			point3 center(a + 0.9 * random_double(), 0.2 + 2 * random_double(), b + 0.9 * random_double());
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
 			if (abs(b) <= 1) {
 				if (-5 <= a && a <= -3 && (center - point3(-4, 1, 0)).length() < 1.9) continue;
@@ -146,7 +146,7 @@ int main()
 	std::cerr << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	//thread
-	int thread_num = 10;
+	int thread_num = 100;
 	vector<thread> threads;
 	vector<string> buffers(thread_num + 1);
 
