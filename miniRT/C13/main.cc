860d1ea99800eb05e3cc5528f0c04c72f6859b84
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-24 18:27:45 +0900

    testing

--- miniRT/C13/main.cc
+++ miniRT/C13/main.cc
@@ -14,11 +14,11 @@
 using namespace std;
 
 // Image
-const auto aspect_ratio = 4.0 / 3.0;
-const int image_width = 400;
+const auto aspect_ratio = 3.0 / 2.0;
+const int image_width = 200;
 const int image_height = static_cast<int>(image_width / aspect_ratio);
-const int samples_per_pixel = 100;
-const int max_depth = 50;
+const int samples_per_pixel = 50;
+const int max_depth = 20;
 auto dist_to_focus = 1.0;
 auto aperture = 0.001;
 
@@ -53,37 +53,37 @@ hittable_list random_scene()
 	{
 		for (int b = -11; b < 11; b++)
 		{
+			point3 center(a + 0.9 * random_double(), 0.2 + 2 * random_double(), b + 0.9 * random_double());
+
+			if (abs(b) <= 1) {
+				if (-5 <= a && a <= -3 && (center - point3(-4, 1, 0)).length() < 1.9) continue;
+				else if (-1 <= a && a <= 1 && (center - point3(0, 1, 0)).length() < 1.9) continue;
+				else if (3 <= a && a <= 5 && (center - point3(4, 1, 0)).length() < 1.9) continue;
+			}
+
 			auto choose_mat = random_double();
-			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+			shared_ptr<material> sphere_material;
 
-			if ((center - point3(4, 0.2, 0)).length() > 0.9
-			&& (center - point3(-4, 0.2, 0)).length() > 0.9
-			&& (center - point3(0, 0.2, 0)).length() > 0.9
-			)
+			if (choose_mat < 0.8)
 			{
-				shared_ptr<material> sphere_material;
-
-				if (choose_mat < 0.8)
-				{
-					// diffuse
-					auto albedo = color::random() * color::random();
-					sphere_material = make_shared<lambertian>(albedo);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-				else if (choose_mat < 0.95)
-				{
-					// metal
-					auto albedo = color::random(0.5, 1);
-					auto fuzz = random_double(0, 0.5);
-					sphere_material = make_shared<metal>(albedo, fuzz);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-				else
-				{
-					// glass
-					sphere_material = make_shared<dielectric>(1.5);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
+				// diffuse
+				auto albedo = color::random() * color::random();
+				sphere_material = make_shared<lambertian>(albedo);
+				world.add(make_shared<sphere>(center, 0.2, sphere_material));
+			}
+			else if (choose_mat < 0.95)
+			{
+				// metal
+				auto albedo = color::random(0.5, 1);
+				auto fuzz = random_double(0, 0.5);
+				sphere_material = make_shared<metal>(albedo, fuzz);
+				world.add(make_shared<sphere>(center, 0.2, sphere_material));
+			}
+			else
+			{
+				// glass
+				sphere_material = make_shared<dielectric>(1.5);
+				world.add(make_shared<sphere>(center, 0.2, sphere_material));
 			}
 		}
 	}
@@ -134,7 +134,7 @@ int main()
 	auto world = random_scene();
 
 	// Camera
-	point3 lookfrom(13, 2, 3);
+	point3 lookfrom(13, 1, 3);
 	point3 lookat(0, 0, 0);
 	vec3 vup(0, 1, 0);
 	dist_to_focus = 13.5;
