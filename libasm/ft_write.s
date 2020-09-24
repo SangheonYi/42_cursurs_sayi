91c852f2ba96a6258e90fe4dccabb77b33cd2a94
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-09-24 14:04:42 +0900

    modified: clean

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -6,7 +6,7 @@ segment .text
 _ft_write:
 	mov r8, rdx				; save rdx = len in r8
 	mov rax, 0x2000004		; set call to write
-    syscall					; call rax (write)
+	syscall					; call rax (write)
 		jc exit_error		; if doesn't work, write set flags carry to 1 so jmp exit error
 	ret
 
