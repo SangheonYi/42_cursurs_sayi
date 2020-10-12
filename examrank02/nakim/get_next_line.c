df8405c264828d608c661a64cd6d0515c921437e
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-10-12 13:19:05 +0900

    modified: exam practice

--- examrank02/nakim/get_next_line.c
+++ examrank02/nakim/get_next_line.c
@@ -1,4 +1,4 @@
-#include "get_next_line.h"
+#include "gnl.h"
 
 int		ft_strlen(char *str)
 {
---
+++ examrank02/nakim/gnl.h
@@ -0,0 +1,10 @@
+#ifndef GNL_H
+#define GNL_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int	gnl(char **line);
+
+#endif
