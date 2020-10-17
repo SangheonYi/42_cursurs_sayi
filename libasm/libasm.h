661507c832e0d927e7ab8919645edb02f9882d55
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2020-10-17 19:50:10 +0900

    modified: norm

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -3,27 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   libasm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sayi <sayi@student.kr>                     +#+  +:+       +#+        */
+/*   By: sayi <sayi@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 14:04:29 by sayi              #+#    #+#             */
-/*   Updated: 2020/09/24 14:04:32 by sayi             ###   ########.fr       */
+/*   Updated: 2020/10/17 19:49:41 by sayi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
-size_t		ft_strlen(const char *str);
-char		*ft_strcpy(char * dst, const char * src);
-int			ft_strcmp(const char *s1, const char *s2);
-ssize_t		ft_write(int fildes, const void *buf, size_t nbyte);
-ssize_t		ft_read(int fildes, void *buf, size_t nbyte);
-char *		ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
 
 #endif
